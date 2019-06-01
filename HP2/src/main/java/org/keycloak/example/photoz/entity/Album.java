6091e8f646b76c54ca996029404a30a36355cab5
larry_lai <leafmouse00@gmail.com>
2019-06-01 08:51:48 +0800

    add all

---
+++ HP2/src/main/java/org/keycloak/example/photoz/entity/Album.java
@@ -0,0 +1,106 @@
+/*
+ * JBoss, Home of Professional Open Source
+ *
+ * Copyright 2015 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.keycloak.example.photoz.entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author <a href="mailto:psilva@redhat.com">Pedro Igor</a>
+ */
+@Entity
+@Table(name="Test3")
+public class Album {
+
+    @Id
+    private String id;
+
+    @Column
+    private String externalId;
+
+    @Column(nullable = false)
+    private String patientId;
+
+    @Column(nullable = false)
+    private String doctorId;
+
+    @Column(nullable = false)
+    private String time;
+
+    @Column(nullable = false)
+    private String detail;
+
+    public String getId() {
+        return this.id;
+    }
+
+    public void setId(final String id) {
+        this.id = id;
+    }
+
+    public void setExternalId(String externalId) {
+        this.externalId = externalId;
+    }
+
+    public String getExternalId() {
+        return externalId;
+    }
+
+    public void setPatientId(String patientId) {
+        this.patientId = patientId;
+    }
+
+    public String getPatientId() {
+        return patientId;
+    }
+
+    public String getDoctorId() {
+        return doctorId;
+    }
+
+    public void setDoctorId(String doctorId) {
+        this.doctorId = doctorId;
+    }
+
+    public String getTime() {
+        return this.time;
+    }
+
+    public void setTime(final String time) {
+        this.time = time;
+    }
+
+    public String getDetail() {
+        return this.detail;
+    }
+
+    public void setDetail(final String detail) {
+        this.detail = detail;
+    }
+
+}
